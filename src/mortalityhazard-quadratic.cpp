bb60444a1e73042a7aec1b38bf72d278cfad5b76
dennis feehan <feehan@berkeley.edu>
2016-06-09 17:49:52 -0700

    new version using erf from RcppFaddeeva

--- src/mortalityhazard-quadratic.cpp
+++ src/mortalityhazard-quadratic.cpp
@@ -9,8 +9,11 @@
  * dennis, dec 2011
  ************************************************/
 
+// [[Rcpp::depends(RcppFaddeeva)]]
+
 #include <complex>
 #include <Rcpp.h>
+#include <RcppFaddeeva.h>
 
 using namespace Rcpp;
 
@@ -42,35 +45,6 @@ NumericVector mortalityhazard_quadratic_cpp(NumericVector theta, NumericVector z
 
 }
 
-std::complex<double> erfi_approx_nr1(std::complex<double> x)
-{
-
-  std::complex<double> erfiof1 (1.65043, 0.0);
-  std::complex<double> expof1 (exp(1.0), 0.0);
-  std::complex<double> sqrtofpi (sqrt(3.1415926535897), 0.0);
-
-  std::complex<double> res (0.0, 0.0);
-
-  res = (173465337818777746628.0*expof1 - 161189343277473823463.0*sqrtofpi*erfiof1 + x*(-181236135883415078228.0*expof1 + 58207050515281020997.0*sqrtofpi*erfiof1 +
-x*(-32990023275939643620.0*expof1 + 70448356034529178041.0*sqrtofpi*erfiof1 +
-x*(53037366273114757940.0*expof1 - 37842691311251513855.0*sqrtofpi*erfiof1 +
-x*(-7666360342517050580.0*expof1 - 5516314131408582425.0*sqrtofpi*erfiof1 +
-x*(-6665334075150129708.0*expof1 + 7528886304748135803.0*sqrtofpi*erfiof1 +
-x*(2371517339152075268.0*expof1 - 1823214457143623537.0*sqrtofpi*erfiof1 +
-x*(-316367854022677700.0*expof1 + 146437170211591039.0*sqrtofpi*erfiof1))))))))/
-   (-161189343277473823463.0*sqrtofpi +
-     x*(58207050515281020997.0*sqrtofpi +
-        x*(70448356034529178041.0*sqrtofpi +
-           x*(-37842691311251513855.0*sqrtofpi +
-              x*(-5516314131408582425.0*sqrtofpi +
-                 x*(7528886304748135803.0*sqrtofpi +
-                    x*(-1823214457143623537.0*sqrtofpi +
-                       146437170211591039.0*sqrtofpi*x)))))));
-
-  return(res);
-
-}
-
 // [[Rcpp::export]]
 NumericVector mortalityhazard_to_prob_quadratic_cpp(NumericVector theta, NumericVector z)
 {
@@ -84,10 +58,10 @@ NumericVector mortalityhazard_to_prob_quadratic_cpp(NumericVector theta, Numeric
   double beta = theta[1];
 
   std::complex<double> gamma = theta[2];
-  std::complex<double> sqrt_gamma = sqrt(gamma);
+  std::complex<double> sqrt_minus_gamma = sqrt(-1.0*gamma);
 
   std::complex<double> k0;
-  k0 = exp(alpha - (pow(beta,2.0)/(4.0*gamma)))*sqrt(pi);
+  k0 = exp(alpha - (pow(beta,2.0)/(4.0*gamma)))*(sqrt(pi) / (2.0 * sqrt_minus_gamma));
 
   // if we're getting Inf or -Inf from this exponential,
   // return NA...
@@ -100,26 +74,138 @@ NumericVector mortalityhazard_to_prob_quadratic_cpp(NumericVector theta, Numeric
 
   std::complex<double> temp;
 
-  Rprintf("log-quadratic pis:\n");
+  std::vector<std::complex<double> > arg1(len), arg2(len);
+
   for(int i=0; i < len; i++) {
-    temp = (k0/(2.0*sqrt_gamma)) *
-      (erfi_approx_nr1((beta + (2.0*gamma*(z[i]+1)))/(2.0*sqrt_gamma)) -
-       erfi_approx_nr1((beta + (2.0*gamma*z[i]))/(2.0*sqrt_gamma)));
 
-    // maddeningly, this:
-    // 1.0 - exp(-1.0*real(temp))
-    // did *not* work!
-    res[i] = real(1.0 - exp(-1.0* temp));
-
-    Rprintf("%f / ", res[i]);
+    arg1[i] = (beta + (2.0*gamma*(z[i]+1)))/(2.0*sqrt_minus_gamma);
+    arg2[i] = (beta + (2.0*gamma*z[i]))/(2.0*sqrt_minus_gamma);
 
   }
-  Rprintf("\n");
+
+  arg1 = RcppFaddeeva::erf(arg1);
+  arg2 = RcppFaddeeva::erf(arg2);
+
+  //Rprintf("log-quadratic pis:\n");
+  for(int i=0; i < len; i++) {
+
+    temp = k0 * (arg2[i] - arg1[i]);
+
+    //Rprintf("%f + i%f / ", real(arg2[i]), imag(arg2[i]));
+
+    temp = 1.0 - exp(-1.0 * temp);
+
+    //Rprintf("%f + i%f / ", real(temp), imag(temp));
+
+    res[i] = real(temp);
+
+    //Rprintf("%f / ", res[i]);
+  }
+
+  //Rprintf("\n");
 
   return(res);
 
 }
 
+//NumericVector mortalityhazard_to_prob_quadratic_cpp(NumericVector theta, NumericVector z)
+//{
+
+//  int len = z.size();
+//  NumericVector res(len);
+
+//  const double pi = 3.1415926535897;
+
+//  double alpha = theta[0];
+//  double beta = theta[1];
+
+//  std::complex<double> gamma = theta[2];
+//  std::complex<double> sqrt_minus_gamma = sqrt(-1.0*gamma);
+
+//  std::complex<double> k0;
+//  k0 = exp(alpha - (pow(beta,2.0)/(4.0*gamma)))*sqrt(pi);
+
+//  // if we're getting Inf or -Inf from this exponential,
+//  // return NA...
+//  if (real(k0) == R_PosInf || real(k0) == R_NegInf) {
+//    for(int i=0; i<len; i++) {
+//      res[i] = NA_INTEGER;
+//    }
+//    return(res);
+//  }
+
+//  std::complex<double> temp;
+
+//  std::vector<std::complex<double> > arg1, arg2;
+
+//  Rprintf("log-quadratic pis:\n");
+//  for(int i=0; i < len; i++) {
+
+//    //temp = (k0/(2.0*sqrt_minus_gamma)) *
+//    //  (RcppFaddeeva::erfi((beta + (2.0*gamma*(z[i]+1)))/(2.0*sqrt_minus_gamma), 0) -
+//    //   RcppFaddeeva::erfi((beta + (2.0*gamma*z[i]))/(2.0*sqrt_minus_gamma)));
+
+//    //arg1[0] = (beta + (2.0*gamma*(z[i]+1)))/(2.0*sqrt_minus_gamma);
+//    //arg2[0] = (beta + (2.0*gamma*z[i]))/(2.0*sqrt_minus_gamma);
+
+//    arg1[0] = (beta + (2.0*gamma*(z[i]+1)))/(2.0*sqrt_minus_gamma);
+//    arg2[0] = (beta + (2.0*gamma*z[i]))/(2.0*sqrt_minus_gamma);
+
+//    Rprintf("arg1: %f", arg1[0]);
+//    Rprintf("arg2: %f", arg2[0]);
+
+//    temp = (k0/(2.0*sqrt_minus_gamma)) *
+//      (RcppFaddeeva::erfi(arg1)[0] -
+//       RcppFaddeeva::erfi(arg2)[0]);
+
+//    // maddeningly, this:
+//    // 1.0 - exp(-1.0*real(temp))
+//    // did *not* work!
+//    res[i] = real(1.0 - exp(-1.0* temp));
+
+//    Rprintf("%f / ", res[i]);
+
+//  }
+
+
+//  Rprintf("\n");
+
+//  return(res);
+
+//}
+
+
+// switching to the RcppFaddeeva pacakge version of the erfi fn
+// rather than my homespun approximation
+//
+//std::complex<double> OLD_erfi_approx_nr1(std::complex<double> x)
+//{
+
+//  std::complex<double> erfiof1 (1.65043, 0.0);
+//  std::complex<double> expof1 (exp(1.0), 0.0);
+//  std::complex<double> sqrtofpi (sqrt(3.1415926535897), 0.0);
+
+//  std::complex<double> res (0.0, 0.0);
+
+//  res = (173465337818777746628.0*expof1 - 161189343277473823463.0*sqrtofpi*erfiof1 + x*(-181236135883415078228.0*expof1 + 58207050515281020997.0*sqrtofpi*erfiof1 +
+//x*(-32990023275939643620.0*expof1 + 70448356034529178041.0*sqrtofpi*erfiof1 +
+//x*(53037366273114757940.0*expof1 - 37842691311251513855.0*sqrtofpi*erfiof1 +
+//x*(-7666360342517050580.0*expof1 - 5516314131408582425.0*sqrtofpi*erfiof1 +
+//x*(-6665334075150129708.0*expof1 + 7528886304748135803.0*sqrtofpi*erfiof1 +
+//x*(2371517339152075268.0*expof1 - 1823214457143623537.0*sqrtofpi*erfiof1 +
+//x*(-316367854022677700.0*expof1 + 146437170211591039.0*sqrtofpi*erfiof1))))))))/
+//   (-161189343277473823463.0*sqrtofpi +
+//     x*(58207050515281020997.0*sqrtofpi +
+//        x*(70448356034529178041.0*sqrtofpi +
+//           x*(-37842691311251513855.0*sqrtofpi +
+//              x*(-5516314131408582425.0*sqrtofpi +
+//                 x*(7528886304748135803.0*sqrtofpi +
+//                    x*(-1823214457143623537.0*sqrtofpi +
+//                       146437170211591039.0*sqrtofpi*x)))))));
+
+//  return(res);
+
+//}
 
 /*
 // use euler's eqn b/c can't find complex-valued exponential
